85c432d5ab7105562c60a6a14ede14f05bb1d318
sonnguyen <nguyencongson163@gmail.com>
2023-10-12 22:38:31 +0700

    init

---
+++ set_env.bat
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=123456aA
+set POSTGRES_HOST=project3-uda-db.cwhqj8ck1p6o.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=arn:aws:s3:::project3-uda
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
